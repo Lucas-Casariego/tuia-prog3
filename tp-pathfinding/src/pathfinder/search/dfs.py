16e6611e4bf5fefbde1335f3fdfa60a50ea6103b
unknown <juarezchiurchiu@gmail.com>
2025-09-25 14:41:39 -0300

    algoritmo dfs tome el bfs y cambié pila por cola

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -19,11 +19,40 @@ class DepthFirstSearch:
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
 
         # Initialize explored with the initial state
-        explored = {}
-        explored[root.state] = True
+        reached = {} # conjunto de estados alcanzados
+        reached[root.state] = True # me marca si el estado fue alcanzado
 
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+#         # Initialize frontier with the root node
+#         # TODO Complete the rest!!
 
-        return NoSolution(explored)
+          # Si la salida coincide con el objetivo..
+        if Grid.objective_test(grid, root.state):
+            return Solution(root, explored, root.cost)
+
+        # la frontera se maneja como una pila 
+        frontier = StackFrontier()
+        frontier.add(root)
+       
+       
+        while not frontier.is_empty():
+            n = frontier.remove()
+            # expandimos el nodo según todas las acciones posibles
+            for action in Grid.actions(grid, n.state):
+                s2 = Grid.result(grid, n.state, action)
+                if s2 not in reached:
+                    n2 = Node("", s2, n.cost + Grid.individual_cost(grid, n.state, action), n, action)
+
+                    if Grid.objective_test(grid, s2):
+                        return Solution(n2, reached, n2.cost)
+
+                    reached[s2] = True
+                    frontier.add(n2)
+
+       
+
+        # Si agotamos la frontera sin hallar solución
+        return NoSolution(reached)
+
+
+
+
