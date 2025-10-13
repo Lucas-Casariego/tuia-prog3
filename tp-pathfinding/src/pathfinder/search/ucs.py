1e6062e22263877829c857b1cc605208662bc77d
Ftedp <facundo.torino7@gmail.com>
2025-10-13 17:34:02 -0300

    Cambio nombre variables ucs

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -29,18 +29,18 @@ class UniformCostSearch:
         frontier.add(root, root.cost)
 
         while not frontier.is_empty():
-            n = frontier.pop()
+            parent_n = frontier.pop()
 
-            if Grid.objective_test(grid, n.state):
-                return Solution(n, reached, n.cost)
+            if Grid.objective_test(grid, parent_n.state):
+                return Solution(parent_n, reached, parent_n.cost)
             
-            for action in Grid.actions(grid, n.state):
-                s2 = Grid.result(grid, n.state, action)
-                c2 = n.cost + Grid.individual_cost(grid, n.state, action)
+            for action in Grid.actions(grid, parent_n.state):
+                new_state = Grid.result(grid, parent_n.state, action)
+                accum_cost = parent_n.cost + Grid.individual_cost(grid, parent_n.state, action)
 
-                if s2 not in reached or c2 < reached[s2]:
-                    n2 = Node("", s2, c2, n, action)
-                    reached[s2] = c2
-                    frontier.add(n2, c2)
+                if new_state not in reached or accum_cost < reached[new_state]:
+                    child_n = Node("", new_state, accum_cost, parent_n, action)
+                    reached[new_state] = accum_cost
+                    frontier.add(child_n, accum_cost)
         
         return NoSolution(reached)
